da886e889dcaddaa4b1ec73cac8d873dbf2c0020
Michael Vetter <jubalh@iodoru.org>
2017-11-20 08:57:05 +0100

    use quick view

--- QmlBook/Ch02/HelloWorld/main.cpp
+++ QmlBook/Ch02/HelloWorld/main.cpp
@@ -5,10 +5,16 @@ int main(int argc, char *argv[])
 {
    QGuiApplication app(argc, argv);
 
+   /*
    QQmlApplicationEngine engine;
    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
    if (engine.rootObjects().isEmpty())
       return -1;
+    */
+
+   QQuickView *view = new QQuickView;
+   view->setSource(QUrl(QStringLiteral("qrc:/main.qml")));
+   view->show();
 
    return app.exec();
 }
